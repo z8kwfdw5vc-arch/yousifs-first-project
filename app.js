fa53e221a7a6769c06c4c84a10fa33e0ccfc4b22
yousif <lunatic.9205@icloud.com>
2026-08-11 18:55:40 +0300

    adding statrting game start  and game timer

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ let timeLeft = 30;
 let timerInterval;
 let isGameRunning;
 let highScore= 0;
+//startin the game
+function startGame() {
+    if (isGameRunning) {
+        return;
+    }
+    isGameRunning = true;
+        startTimer();
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        isGameRunning = false;
+    });
+}
+
