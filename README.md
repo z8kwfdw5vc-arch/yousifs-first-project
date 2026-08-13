3486166fa84464035c776d024bf36ca92d514092
yousif <lunatic.9205@icloud.com>
2026-08-13 20:19:33 +0300

    add a navbar with links and seconed target

---
+++ README.md
@@ -0,0 +1,28 @@
+# Project Name
+CLICK THE TARGET game
+
+## Technologies Used
+HTML
+css
+java script
+## Description
+a simple game with a moving target whitch the user got to click it and get a higher score, like makeing a competiton to get the higher score its depinding on reflection and how fast the player can go
+## User Stories
+*As a user i want to click the start botton to start the game.
+*As a user i want the target to change the position randomly and catch many of it
+*as a user my score to incris after every time a catch the target
+*as a user i want the game to stop automaticly when the time hits zero(0)
+as i user i want a reset button to clear everything and start a new game
+
+## Screenshots
+
+
+
+
+
+## Future Enhancements
+1.Adding a local storege so it let the player to save all his recoreds and make a compation for the highest socore and the fastest player.
+2.Adding more then one character animation.
+3.Adding some vioces as reactions for the clicking and for the target.
+## Credits
+1-i would like to thank our instructers : OMER, Jaeela, AND Sayed Hamed for teaching supporting snd keep us motavaited.
--- app.js
+++ app.js
@@ -5,17 +5,23 @@ let score = 0;
 let timeLeft = 30;
 let timerInterval;
 let isGameRunning = false;
-let highScore= 0;
+
+
 /*------------------------ Cached Element References ------------------------*/
-const targetElement = document.querySelector('#target');
+const targetElement = document.querySelector('#target')
 const scoreElement = document.querySelector('#score-display');
 const timerElement = document.querySelector('#timer-display');
 const startButtonElement = document.querySelector('#start-btn');
 const resetButtonElement = document.querySelector('#reset-btn');
 /*-------------------------------- Functions --------------------------------*/
 function startGame() {
-    if (isGameRunning) return;
+    console.log("isGamingRunninng", "timeLeft", timeLeft);
+    if (isGameRunning) {
+        console.log("isGamingRunninng");
+            return;
+        }   
     if (timeLeft === 0) {
+        console.log(timeLeft)
         score = 0;
         timeLeft = 30;
         scoreElement.textContent = '0';
@@ -54,6 +60,8 @@ function startTimer() {
         targetElement.style.left = randomX + 'px';
         targetElement.style.top = randomY + 'px';
     }
+    
+    
     function resetGame() {
         clearInterval(timerInterval);
         isGameRunning = false;
@@ -63,8 +71,10 @@ function startTimer() {
         scoreElement.textContent = '0';
         timerElement.textContent = '30';
         targetElement.style.left = '225px'; // 550/2=225px
-        targetElement.style.top = '175px'; // 350/2=175px
+        targetElement.style.top = '175px'; // 350/2=175px 
     }
+
+    
 //event listern
 targetElement.addEventListener('click', handleTargetClick);
 startButtonElement.addEventListener('click', startGame);
